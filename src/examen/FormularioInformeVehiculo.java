c2b45218042914a6acef23f4665491039c5531e9
Esteban Cruz <esteban.09.cr@gmail.com>
2019-11-03 01:16:54 -0600

    Boton

--- src/examen/FormularioInformeVehiculo.java
+++ src/examen/FormularioInformeVehiculo.java
@@ -1,8 +1,25 @@
 
 package examen;
+import javax.swing.ButtonGroup;
+import javax.swing.JRadioButton;
+
 
 public class FormularioInformeVehiculo {
+
+private void inicializarInformeVehiculo(){
     
+   JRadioButton rbtn1=new JRadioButton("Reparado",true);
+   JRadioButton rbtn2=new JRadioButton("En Reparacion",false);
+   JRadioButton rbtn3=new JRadioButton("Mostrar",true);
+   
+   ButtonGroup grupo1 = new ButtonGroup();
+   grupo1.add(rbtn1);
+   grupo1.add(rbtn2);
+   grupo1.add(rbtn3);
+ 
 }
+ } 
+    
+
     
   
